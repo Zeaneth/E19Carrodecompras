e48a113a6bfafd41cba6cd766397943ed3448d6a
Zamiz <zamiztobar@gmail.com>
2019-01-21 22:13:43 -0300

    feat(controller): add orders create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def create
+    @product = Product.find(params[:product_id])
+    @order = Order.new(product: @product, user: current_user)
+    if @order.save
+      redirect_to products_path, notice: 'La orden ha sido ingresada'
+    else
+      redirect_to products_path, alert: 'La orden no ha podido ser ingresada'
+    end
   end
 end
