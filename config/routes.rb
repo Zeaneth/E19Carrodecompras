9062f124c2c768cd8c7c4b9623563bb7af6a9ab5
Zamiz <zamiztobar@gmail.com>
2019-01-21 22:39:55 -0300

    feat(routes): add order index routefeat(index): add orders path to purchase order link on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :orders, only: :create
   end
 
+
+  resources :orders, only: :index
   root to: 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
